bee132827cfa66a4300065e7a1661fb2ba5bdcf5
DiyarYelbaka <yelbaka18@gmail.com>
2023-02-01 13:50:12 +0300

    BuildAppSettings

--- android/app/src/main/java/com/chatapp/MainActivity.java
+++ android/app/src/main/java/com/chatapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.chatapp;
+package com.chatappdimetra;
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- android/app/src/main/java/com/chatapp/MainApplication.java
+++ android/app/src/main/java/com/chatapp/MainApplication.java
@@ -1,4 +1,4 @@
-package com.chatapp;
+package com.chatappdimetra;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
